a4e910faf504607406fb726b328ffa6f77a93e96
raphauy <rapha.uy@rapha.uy>
2025-07-02 15:08:14 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import { ConvexClientProvider } from "./convex-client-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Smart Notes",
+    default: "Smart Notes",
+  },
+  description:
+    "A note-taking app with AI chatbot integration built with Convex and the Vercel AI SDK.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.className} antialiased`}>
+        <ConvexClientProvider>
+          {children}
+        </ConvexClientProvider>
+        <Toaster position="top-right" />
+        </body>
+    </html>
+  );
+}
